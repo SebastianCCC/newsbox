df94282c1951882098fbd776de9809e1fc022a96
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-07-01 17:06:51 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
+
+const babel = require('gulp-babel')
+
+function buildStyles() {
+  return (
+    src('sass/**/*.scss')
+      .pipe(sass({ outputStyle: 'compressed' }))
+      /*     .pipe(purgecss({ content: ['*.html'] })) */
+      .pipe(dest('dist'))
+  )
+}
+
+function buildJavaScript() {
+  return src('js/**/*.js')
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+      })
+    )
+    .pipe(dest('dist'))
+}
+
+function watchTask() {
+  watch(['sass/**/*.scss', '*.html', 'js/**/*.js'], series(buildStyles, buildJavaScript))
+}
+
+exports.default = series(buildStyles, buildJavaScript)
---
+++ js/delete.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('#displaynews')
+  let archiveArray = []
+
+  if (localStorage.getItem('archive')) {
+    archiveArray = JSON.parse(localStorage.getItem('archive'))
+    console.log(archiveArray)
+  }
+
+  container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('displaynews-savebutton')) {
+      save(event.target.parentNode.id)
+    } else if (event.target.classList.contains('displaynews-savebutton-icon')) {
+      save(event.target.parentNode.parentNode.id)
+    }
+  })
+
+  function save(elementId) {
+    let article = document.querySelector(`#${elementId}`)
+    archiveArray = archiveArray.filter((article) => article.id != elementId)
+
+    localStorage.setItem('archive', JSON.stringify(archiveArray))
+
+    article.classList.add('animate__animated', 'animate__fadeOutLeft', 'animate__fast')
+    setTimeout(() => {
+      article.parentNode.removeChild(article)
+    }, 800)
+  }
+})
